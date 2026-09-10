7942a85668e4984e0fe124938c458a8caf5c82a8
Rayson Chong <rcsf2004@gmail.com>
2026-09-10 23:16:35 +0800

    Program loop until quit

--- auditor.py
+++ auditor.py
@@ -1,2 +1,11 @@
 # Initialize the inventory to zero in the start
 inventory = 0
+
+# Run in a continuous loop asking user to enter a stock quantity, until the user types quit.
+while True:
+    stock_input = input("Enter stock quantity (or enter 'quit' to exit program): ")
+
+    # Exit the program if user types quit
+    if stock_input.lower() == "quit":
+        break
+
